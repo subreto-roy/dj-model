d4349229321485ee5edd0051572455a82e541540
subreto-roy <sroy172182@bscse.uiu.ac.bd>
2023-04-10 14:34:07 +0600

    Database

--- enroll/admin.py
+++ enroll/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from enroll.models import Student
 
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display=('id','stuid','stuname','stuemail','stupass')
+    
 # Register your models here.
+#admin.site.register(Student,StudentAdmin)

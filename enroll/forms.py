8f6fb7644c8fb25f7a578c9505e2d0c12d28c674
subreto-roy <sroy172182@bscse.uiu.ac.bd>
2023-04-12 15:17:56 +0600

    request

--- enroll/forms.py
+++ enroll/forms.py
@@ -3,3 +3,5 @@ from django import forms
 class StudentRegistration(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
+    mobile = forms.IntegerField()
+    key = forms.CharField(widget=forms.HiddenInput())

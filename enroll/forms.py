97a1b14c25a77287b3fd1b8e491a865eb100518f
subreto-roy <sroy172182@bscse.uiu.ac.bd>
2023-04-14 13:00:59 +0600

    Form

--- enroll/forms.py
+++ enroll/forms.py
@@ -1,14 +1,19 @@
 from django import forms
+from django.core import validators
 
-
+def start_with_S(value):
+    if value[0] != 's':
+        raise forms.ValidationError('error')
+    
+    
 class StudentRegistration(forms.Form):
-    name = forms.CharField()
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    name = forms.CharField(validators=[start_with_S])
+    email = forms.EmailField(validators=[start_with_S])
+    #password = forms.CharField(widget=forms.PasswordInput)
     # key = forms.CharField(widget=forms.HiddenInput())
 
-    def clean_name(self):
-        valname = self.changed_data['name']
-        if len(valname) < 4:
-            raise forms.ValidationError('Enter more than or equal 4')
-        return valname
+    # def clean_name(self):
+    #     valname = self.changed_data['name']
+    #     if len(valname) < 4:
+    #         raise forms.ValidationError('Enter more than or equal 4')
+    #     return valname
--- enroll/views.py
+++ enroll/views.py
@@ -6,14 +6,21 @@ def showformdata(request):
     if request.method == 'POST':
         fm = StudentRegistration(request.POST)
         if fm.is_valid():
-            name = fm.cleaned_data['name']
-            email = fm.cleaned_data['email']
-            password = fm.cleaned_data['password']
-            print('Name:', name)
+            print('Form Validated')
+            print('Name:', fm.cleaned_data['name'])
+            print('Email:', fm.cleaned_data['email'])
+            # name = fm.cleaned_data['name']
+            # email = fm.cleaned_data['email']
+            # password = fm.cleaned_data['password']
+            # print('Name:', name)
             #return render(request,'enroll/success.html', {'nm':name})
         else:
             fm = StudentRegistration()
-        return render(request, 'enroll/userregistration.html', {'form': fm})
+        
+    else:
+        fm=StudentRegistration()
+        
+    return render(request, 'enroll/userregistration.html', {'form': fm})
 
 
 # def showformdata(request):
